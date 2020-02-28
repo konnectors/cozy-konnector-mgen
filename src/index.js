e8b3a8f2723f6b8f2a4007ddf0d14b54ceaddb7d
doubleface <christophe@cozycloud.cc>
2020-02-28 15:10:38 +0100

    fix: handle bill detail html change + async/await

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ const {
   BaseKonnector,
   requestFactory,
   log,
-  saveFiles,
-  saveBills,
   errors
 } = require('cozy-konnector-libs')
 const moment = require('moment')
@@ -36,23 +34,18 @@ async function start(fields) {
   await connector.fetchAttestationMutuelle(fields, this)
   const entries = await connector.fetchReimbursements()
   if (entries !== false) {
-    await saveBills(entries, fields.folderPath, {
-      identifiers: 'MGEN',
-      sourceAccount: this.accountId,
-      sourceAccountIdentifier: fields.login,
+    await this.saveBills(entries, fields, {
       linkBankOperations: false,
       fileIdAttributes: ['filename']
     })
   } else {
-    log('info', 'No need to save Bills')
+    log('debug', 'No need to save Bills')
   }
 }
 
-connector.logIn = function(fields) {
-  log('info', 'Logging in')
-  return request({
-    url: 'https://www.mgen.fr/login-adherent/',
-    method: 'POST',
+connector.logIn = async function(fields) {
+  log('debug', 'Logging in')
+  const response = await request.post('https://www.mgen.fr/login-adherent/', {
     formData: {
       typeConnexion: 'adherent',
       user: fields.login,
@@ -61,43 +54,41 @@ connector.logIn = function(fields) {
       redirect_url: '/mon-espace-perso/'
     },
     resolveWithFullResponse: true
-  }).then(response => {
-    if (response.request.uri.pathname === '/services-indisponibles/') {
-      throw new Error(errors.VENDOR_DOWN)
-    }
-
-    const $ = response.body
-
-    if ($('.tx-felogin-pi1').length > 0) {
-      const errorMessage = $('.tx-felogin-pi1 .alert-danger')
-        .text()
-        .trim()
-      log('error', errorMessage)
-      if (errorMessage.includes('le compte a été bloqué')) {
-        throw new Error('LOGIN_FAILED.TOO_MANY_ATTEMPTS')
-      }
-      throw new Error(errors.LOGIN_FAILED)
-    }
-
-    if ($.text().includes('Mgen simplifie votre connexion !')) {
-      log('warn', 'MGEN asks to change the login to an email')
-      throw new Error(errors.USER_ACTION_NEEDED)
-    }
-
-    return $
   })
+
+  if (response.request.uri.pathname === '/services-indisponibles/') {
+    throw new Error(errors.VENDOR_DOWN)
+  }
+
+  const $ = response.body
+
+  if ($('.tx-felogin-pi1').length > 0) {
+    const errorMessage = $('.tx-felogin-pi1 .alert-danger')
+      .text()
+      .trim()
+    log('error', errorMessage)
+    if (errorMessage.includes('le compte a été bloqué')) {
+      throw new Error('LOGIN_FAILED.TOO_MANY_ATTEMPTS')
+    }
+    throw new Error(errors.LOGIN_FAILED)
+  }
+
+  if ($.text().includes('Mgen simplifie votre connexion !')) {
+    log('warn', 'MGEN asks to change the login to an email')
+    throw new Error(errors.USER_ACTION_NEEDED)
+  }
+
+  return $
 }
 
-connector.fetchCards = function() {
+connector.fetchCards = async function() {
   // first fetches profilage data or else the next request won't work
-  return request({
-    url: `${baseUrl}/mon-espace-perso/?type=30303&_=${new Date().getTime()}`,
-    json: true
-  }).then(() =>
-    request(
-      'https://www.mgen.fr/mon-espace-perso/?type=30304&_=' +
-        new Date().getTime()
-    )
+  await request.get(
+    `${baseUrl}/mon-espace-perso/?type=30303&_=${new Date().getTime()}`,
+    { json: true }
+  )
+  return await request.get(
+    'https://www.mgen.fr/mon-espace-perso/?type=30304&_=' + new Date().getTime()
   )
 }
 
@@ -117,89 +108,84 @@ const addGroupAmounts = entries => {
   })
 }
 
-connector.fetchReimbursements = function() {
-  log('info', 'Fetching reimbursements')
-  const url = 'https://www.mgen.fr/mon-espace-perso/mes-remboursements/'
+connector.fetchReimbursements = async function() {
+  log('debug', 'Fetching reimbursements')
+  const $ = await request(
+    'https://www.mgen.fr/mon-espace-perso/mes-remboursements/'
+  )
+  log(
+    'debug',
+    `Expect beneficiaries : ${$('#remboursement_listebenef option').length}`
+  )
 
-  return request(url).then($ => {
+  if (
+    $.html().includes('avez pas de remboursement pour les six derniers mois')
+  ) {
     log(
-      'debug',
-      `Expect beneficiaries : ${$('#remboursement_listebenef option').length}`
+      'warn',
+      "No bills, we found 'avez pas de remboursement pour les six derniers mois' in html"
     )
+    // This false will be catch and saveBills not execute
+    return false
+  }
+  // Initialise some form Data for all following POST (pdf and details)
+  const $formDetails = $('#formDetailsRemboursement')
+  const formData = serializedFormToFormData($formDetails.serializeArray())
 
-    if (
-      $.html().includes('avez pas de remboursement pour les six derniers mois')
-    ) {
-      log(
-        'warn',
-        "No bills, we found 'avez pas de remboursement pour les six derniers mois' in html"
-      )
-      // This false will be catch and saveBills not execute
-      return false
+  // table parsing
+  let entries = Array.from($('#tableDernierRemboursement tbody tr')).map(tr => {
+    const tds = Array.from($(tr).find('td')).map(td => {
+      return $(td)
+        .text()
+        .trim()
+    })
+
+    const date = moment(tds[4], 'DD/MM/YYYY')
+    const entry = {
+      type: 'health_costs',
+      vendor: 'MGEN',
+      isRefund: true,
+      indexLine: tds[0], // removed later
+      originalDate: moment(tds[1], 'DD/MM/YYYY').toDate(),
+      beneficiary: tds[2],
+      date: date.toDate()
     }
-    // Initialise some form Data for all following POST (pdf and details)
-    const $formDetails = $('#formDetailsRemboursement')
-    const formData = serializedFormToFormData($formDetails.serializeArray())
 
-    // table parsing
-    let entries = Array.from($('#tableDernierRemboursement tbody tr')).map(
-      tr => {
-        const tds = Array.from($(tr).find('td')).map(td => {
-          return $(td)
-            .text()
-            .trim()
-        })
-
-        const date = moment(tds[4], 'DD/MM/YYYY')
-        const entry = {
-          type: 'health_costs',
-          vendor: 'MGEN',
-          isRefund: true,
-          indexLine: tds[0], // removed later
-          originalDate: moment(tds[1], 'DD/MM/YYYY').toDate(),
-          beneficiary: tds[2],
-          date: date.toDate()
+    const $pdfLink = $(tr).find('.pdf_download')
+    if ($pdfLink.length) {
+      entry.fileurl = baseUrl + unescape($pdfLink.attr('href'))
+      const parsedUrl = querystring.decode(entry.fileurl)
+      entry.filename = `${moment(parsedUrl.dateReleve).format(
+        'YYYY-MM-DD'
+      )}_mgen.pdf`
+      entry.requestOptions = {
+        method: 'POST',
+        form: {
+          ...formData,
+          urlReleve: parsedUrl.urlReleve,
+          dattrait: parsedUrl.dattrait,
+          dateReleve: parsedUrl.dateReleve
         }
-
-        const $pdfLink = $(tr).find('.pdf_download')
-        if ($pdfLink.length) {
-          entry.fileurl = baseUrl + unescape($pdfLink.attr('href'))
-          const parsedUrl = querystring.decode(entry.fileurl)
-          entry.filename = `${moment(parsedUrl.dateReleve).format(
-            'YYYY-MM-DD'
-          )}_mgen.pdf`
-          entry.requestOptions = {
-            method: 'POST',
-            form: {
-              ...formData,
-              urlReleve: parsedUrl.urlReleve,
-              dattrait: parsedUrl.dattrait,
-              dateReleve: parsedUrl.dateReleve
-            }
-          }
-        }
-
-        return entry
       }
-    )
+    }
 
-    // Initialize some form Data for fecthing details
-    const propName =
-      'tx_mtechremboursementxmlhttp_mtechremboursementsantexmlhttp[rowIdOrder]'
-    formData[propName] = entries.map(entry => entry.indexLine).join(',')
-    const action = unescape($formDetails.attr('action'))
-
-    return bluebird
-      .map(
-        entries,
-        entry => connector.fetchDetailsReimbursement(entry, action, formData),
-        { concurrency: 5 }
-      )
-      .then(entries => {
-        addGroupAmounts(entries)
-        return entries
-      })
+    return entry
   })
+
+  // Initialize some form Data for fecthing details
+  const propName =
+    'tx_mtechremboursementxmlhttp_mtechremboursementsantexmlhttp[rowIdOrder]'
+  formData[propName] = entries.map(entry => entry.indexLine).join(',')
+  const action = unescape($formDetails.attr('action'))
+
+  entries = await bluebird.map(
+    entries,
+    entry => connector.fetchDetailsReimbursement(entry, action, formData),
+    { concurrency: 5 }
+  )
+
+  addGroupAmounts(entries)
+  return entries
 }
 
 // convert a string amount to a float
@@ -212,75 +198,70 @@ function convertAmount(amount) {
   )
 }
 
-connector.fetchDetailsReimbursement = function(entry, action, formData) {
-  log('info', `Fetching details for line ${entry.indexLine}`)
+connector.fetchDetailsReimbursement = async function(entry, action, formData) {
+  log('debug', `Fetching details for line ${entry.indexLine}`)
   formData['tx_mtechremboursement_mtechremboursementsante[indexLigne]'] =
     entry.indexLine
-  return request({
-    url: baseUrl + action,
-    method: 'POST',
-    form: formData
-  }).then($ => {
-    const $tables = $('#ajax-details-remboursements table')
-    const $tableSummary = $tables.eq(0)
-    const $tableDetails = $tables.eq(1)
-    const data = Array.from($tableSummary.find('tr')).reduce((memo, tr) => {
-      const $tds = $(tr).find('td')
-      const name = $tds
+  const $ = await request.post(baseUrl + action, { form: formData })
+  const $tables = $('#ajax-details-remboursements table')
+  const $tableSummary = $tables.eq(0)
+  const $tableDetails = $tables.eq(1)
+  const data = Array.from($tableSummary.find('tr')).reduce((memo, tr) => {
+    const $tds = $(tr).find('th,td')
+    const name = $tds
+      .eq(0)
+      .text()
+      .trim()
+    memo[name] = $tds
+      .eq(1)
+      .text()
+      .trim()
+    return memo
+  }, {})
+
+  entry.originalAmount = convertAmount(data['Montant des soins'])
+
+  // not used anymore
+  delete entry.indexLine
+
+  const details = Array.from($tableDetails.find('tbody tr')).map(tr => {
+    const $tds = $(tr).find('td')
+    return {
+      designation: $tds
         .eq(0)
         .text()
-        .trim()
-      memo[name] = $tds
-        .eq(1)
-        .text()
-        .trim()
-      return memo
-    }, {})
-
-    entry.originalAmount = convertAmount(data['Montant des soins'])
-
-    // not used anymore
-    delete entry.indexLine
-
-    const details = Array.from($tableDetails.find('tbody tr')).map(tr => {
-      const $tds = $(tr).find('td')
-      return {
-        designation: $tds
-          .eq(0)
-          .text()
-          .trim(),
-        reimbursementSS: convertAmount($tds.eq(2).text()),
-        reimbursementMGEN: convertAmount($tds.eq(3).text())
-      }
-    })
-
-    if (data["Remboursement à l'assuré"] === '0,00 €') {
-      entry.isThirdPartyPayer = true
+        .trim(),
+      reimbursementSS: convertAmount($tds.eq(2).text()),
+      reimbursementMGEN: convertAmount($tds.eq(3).text())
     }
-
-    // get data from the details table
-    const sums = details.reduce(
-      (memo, detail) => {
-        memo.designation.push(detail.designation)
-        memo.reimbursementSS += detail.reimbursementSS
-        memo.reimbursementMGEN += detail.reimbursementMGEN
-        return memo
-      },
-      { designation: [], reimbursementSS: 0, reimbursementMGEN: 0 }
-    )
-    entry.amount = convertAmount(data["Remboursement à l'assuré"])
-    // remove duplicates
-    sums.designation = Array.from(new Set(sums.designation))
-    entry.subtype = sums.designation.join(', ')
-    entry.socialSecurityRefund = round(sums.reimbursementSS)
-    entry.thirdPartyRefund = round(sums.reimbursementMGEN)
-
-    return entry
   })
+
+  if (data["Remboursement à l'assuré"] === '0,00 €') {
+    entry.isThirdPartyPayer = true
+  }
+
+  // get data from the details table
+  const sums = details.reduce(
+    (memo, detail) => {
+      memo.designation.push(detail.designation)
+      memo.reimbursementSS += detail.reimbursementSS
+      memo.reimbursementMGEN += detail.reimbursementMGEN
+      return memo
+    },
+    { designation: [], reimbursementSS: 0, reimbursementMGEN: 0 }
+  )
+  entry.amount = convertAmount(data["Remboursement à l'assuré"])
+  // remove duplicates
+  sums.designation = Array.from(new Set(sums.designation))
+  entry.subtype = sums.designation.join(', ')
+  entry.socialSecurityRefund = round(sums.reimbursementSS)
+  entry.thirdPartyRefund = round(sums.reimbursementMGEN)
+
+  return entry
 }
 
 connector.fetchAttestationMutuelle = async function(fields, self) {
-  log('info', 'Fetching mutuelle attestation')
+  log('debug', 'Fetching mutuelle attestation')
   try {
     const $ = await request(
       'https://www.mgen.fr/mon-espace-perso/ma-carte-adherent/'
@@ -288,7 +269,6 @@ connector.fetchAttestationMutuelle = async function(fields, self) {
     const $formDetails = $('.formCarteAdhRCPdf')
     const formData = serializedFormToFormData($formDetails.serializeArray())
     const linkPost = $('.formCarteAdhRCPdf').attr('action')
-    log('debug', linkPost, 'linkPost')
     const script = $('.carte-adherent-entete')
       .prev('script')
       .html()
@@ -296,8 +276,6 @@ connector.fetchAttestationMutuelle = async function(fields, self) {
       const linkGet = script.match(
         /actionTelechargementCarteAdherentPdf = '(.*)'/
       )[1]
-      log('debug', linkGet, 'linkGet')
-
       // This request is mandatory for the GET of saveFiles
       await request({
         uri: baseUrl + linkPost,
@@ -311,9 +289,7 @@ connector.fetchAttestationMutuelle = async function(fields, self) {
         filename: 'Attestation_mutuelle.pdf',
         shouldReplaceFile: () => true
       }
-      await saveFiles([entry], fields, {
-        sourceAccount: self._account._id,
-        sourceAccountIdentifier: fields.login,
+      await self.saveFiles([entry], fields, {
         fileIdAttributes: ['filename']
       })
     } else {
@@ -354,9 +330,8 @@ connector.fetchAttestationMutuelleObligatoire = async function(fields, self) {
         filename: 'Attestation_mutuelle.pdf',
         shouldReplaceFile: () => true
       }
-      await saveFiles([entry], fields, {
-        sourceAccount: self.accountId,
-        sourceAccountIdentifier: fields.login
+      await self.saveFiles([entry], fields, {
+        fileIdAttributes: ['filename']
       })
     } else {
       log('warn', 'No attestation to fetch')
